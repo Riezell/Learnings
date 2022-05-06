2b277c0686fe3b39ad43e77a578c0a210301b30b
Riezell <riezellfanny08@gmail.com>
2022-05-06 21:34:07 +0800

    commit

---
+++ 8-exercise/src/com/project/AreaCalculatorMain.java
@@ -0,0 +1,14 @@
+package com.project;
+
+public class AreaCalculatorMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("First circle area: " + AreaCalculator.area(5.0));
+		System.out.println("Second circle area: " + AreaCalculator.area(-1));
+		System.out.println("Rectangle are number1: " + AreaCalculator.area(5.0, 4.0));
+		System.out.println("Rectangle are number2: " + AreaCalculator.area(-1.0, 4.0));
+
+	}
+
+}
