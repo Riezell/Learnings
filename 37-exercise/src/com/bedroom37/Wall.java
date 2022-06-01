8dd9c8ec9e68a9df6402d657455deea0c161fe85
Riezell <riezellfanny08@gmail.com>
2022-06-01 18:21:32 +0800

    Commit

---
+++ 37-exercise/src/com/bedroom37/Wall.java
@@ -0,0 +1,19 @@
+package com.bedroom37;
+
+public class Wall {
+	private String direction;
+
+	public Wall(String direction) {
+		super();
+		this.direction = direction;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+	
+	
+	
+	
+
+}
